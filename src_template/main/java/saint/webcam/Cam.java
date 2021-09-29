9e5cecd356ef51cb38ebe51544af308bcc4617eb
Jared Gridley <87240032+JGridleyMLDL@users.noreply.github.com>
2021-09-28 23:03:13 -0400

    Update Cam.java
    Print Status

--- src_template/main/java/saint/webcam/Cam.java
+++ src_template/main/java/saint/webcam/Cam.java
@@ -42,7 +42,7 @@ public class Cam {
     public static void Capture(String filePath, String fileName, int widthx, int heighty) throws IOException {
         Webcam webcam = Webcam.getDefault();
         if (webcam != null) {
-//            System.out.println("Webcam: " + webcam.getName());
+            System.out.println("Webcam: " + webcam.getName());
             webcam.setViewSize(new Dimension(widthx, heighty));
             webcam.open();
             ImageIO.write(webcam.getImage(), "PNG", new File(filePath + fileName + ".png"));
